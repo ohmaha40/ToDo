7549e2d95b09b8d3a019f96ccbb8793f2b0f795e
Ohmaha40 <keller.chris@outlook.de>
2022-11-12 22:18:08 +0100

    Tabelle angepasst und ersten insert

--- gui.py
+++ gui.py
@@ -1,8 +1,8 @@
 import sqlite3
-
 from anzeige.frm_main import Ui_startfenster
-from PySide6 import QtSql
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6 import QtSql, QtWidgets
+from PySide6.QtWidgets import QApplication, QMainWindow, QDialog
+from datetime import date
 app = QApplication()
 db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
 db.setDatabaseName("db/database.db")
@@ -19,25 +19,36 @@ class Start_Fenster(QMainWindow, Ui_startfenster):
         self.tv_todo.setColumnWidth(2, 640)
         self.tv_todo.setColumnWidth(3, 80)
         self.tv_todo.setColumnWidth(4, 80)
-
+        #self.test_funktionen()("Datum", "Zu erledigen", "Bis wann", "Dringend")
 
     def todo_eintragen(self):
-        text = self.input_text_todo.text()
         conn = sqlite3.connect("db/database.db")
         cursor = conn.cursor()
-        sql = "INSERT INTO todo_liste (Datum, [zu erledigen]) VALUES ('16.11.2022', ?)"
-        cursor.execute(sql, (text,))
+        sql = "INSERT INTO todo_liste (Datum, [zu erledigen], bis_Datum, Dringlichkeit) VALUES (?, ?, ?, ?)"
+        cursor.execute(sql, (date.today().strftime('%d.%m.%Y'), self.input_text_todo.text(), self.date_end_todo.date().toString(("dd.MM.yyyy")), self.cb_dringlichkeit.currentText(),))
         conn.commit()
         conn.close()
         self.input_text_todo.setText("")
         self.todo_aktualisieren()
-
-
-
     def todo_aktualisieren(self):
         model = self.mod_todo
-        model.select()
+        sqlquery = "SELECT * FROM todo_liste"
+        model.itemData(sqlquery)
         self.tv_todo.setModel(model)
+        # conn = sqlite3.connect("db/database.db")
+        # cursor = conn.cursor()
+        # sqlquery = "SELECT * FROM todo_liste"
+        # tablerow = 0
+        # for row in cursor.execute(sqlquery):
+        #     self.tv_todo.(tablerow, 0, QtWidgets.QTableWidgetItem(row[0]))
+        #
+        #     tablerow+=1
+        #     print(row)
+        # model = self.mod_todo
+        # model.selectRow()ct()
+        # self.tv_todo.setModel(model)
+    # def test_funktionen(self):
+    #     print(self.cb_dringlichkeit.currentText())
 
 
 start_fenster = Start_Fenster()
