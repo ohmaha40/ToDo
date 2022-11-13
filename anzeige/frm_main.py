6489a3fcc8e6fbbdd90b9674ed956cd23097db72
Ohmaha40 <keller.chris@outlook.de>
2022-11-13 21:36:02 +0100

    erweiterungen

--- anzeige/frm_main.py
+++ anzeige/frm_main.py
@@ -17,14 +17,15 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QComboBox, QDateEdit,
     QHeaderView, QLineEdit, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QTableView,
-    QWidget)
+    QPushButton, QSizePolicy, QStatusBar, QTabWidget,
+    QTableWidget, QTableWidgetItem, QWidget)
 
 class Ui_startfenster(object):
     def setupUi(self, startfenster):
         if not startfenster.objectName():
             startfenster.setObjectName(u"startfenster")
         startfenster.resize(1000, 600)
+        startfenster.setTabShape(QTabWidget.Rounded)
         self.centralwidget = QWidget(startfenster)
         self.centralwidget.setObjectName(u"centralwidget")
         self.input_text_todo = QLineEdit(self.centralwidget)
@@ -38,7 +39,7 @@ class Ui_startfenster(object):
         self.date_end_todo.setLayoutDirection(Qt.LeftToRight)
         self.date_end_todo.setAlignment(Qt.AlignCenter)
         self.date_end_todo.setButtonSymbols(QAbstractSpinBox.UpDownArrows)
-        self.date_end_todo.setDateTime(QDateTime(QDate(2022, 10, 13), QTime(18, 0, 0)))
+        self.date_end_todo.setDateTime(QDateTime(QDate(2022, 10, 13), QTime(12, 0, 0)))
         self.date_end_todo.setMinimumDate(QDate(2022, 10, 11))
         self.date_end_todo.setCalendarPopup(True)
         self.cb_dringlichkeit = QComboBox(self.centralwidget)
@@ -48,15 +49,27 @@ class Ui_startfenster(object):
         self.cb_dringlichkeit.setObjectName(u"cb_dringlichkeit")
         self.cb_dringlichkeit.setGeometry(QRect(390, 90, 111, 22))
         self.cb_dringlichkeit.setEditable(True)
-        self.tv_todo = QTableView(self.centralwidget)
-        self.tv_todo.setObjectName(u"tv_todo")
-        self.tv_todo.setGeometry(QRect(30, 170, 941, 381))
         self.pb_todo_eintragen = QPushButton(self.centralwidget)
         self.pb_todo_eintragen.setObjectName(u"pb_todo_eintragen")
         self.pb_todo_eintragen.setGeometry(QRect(30, 120, 351, 24))
         self.pb_Projekte = QPushButton(self.centralwidget)
         self.pb_Projekte.setObjectName(u"pb_Projekte")
         self.pb_Projekte.setGeometry(QRect(550, 70, 411, 24))
+        self.tv_todo = QTableWidget(self.centralwidget)
+        if (self.tv_todo.columnCount() < 5):
+            self.tv_todo.setColumnCount(5)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tv_todo.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tv_todo.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tv_todo.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tv_todo.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tv_todo.setHorizontalHeaderItem(4, __qtablewidgetitem4)
+        self.tv_todo.setObjectName(u"tv_todo")
+        self.tv_todo.setGeometry(QRect(30, 160, 931, 391))
         startfenster.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(startfenster)
         self.menubar.setObjectName(u"menubar")
@@ -79,5 +92,15 @@ class Ui_startfenster(object):
 
         self.pb_todo_eintragen.setText(QCoreApplication.translate("startfenster", u"Todo eintragen", None))
         self.pb_Projekte.setText(QCoreApplication.translate("startfenster", u"Projekte", None))
+        ___qtablewidgetitem = self.tv_todo.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("startfenster", u"spalte 1", None));
+        ___qtablewidgetitem1 = self.tv_todo.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("startfenster", u"spalte 2", None));
+        ___qtablewidgetitem2 = self.tv_todo.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("startfenster", u"spalte 3", None));
+        ___qtablewidgetitem3 = self.tv_todo.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("startfenster", u"spalte 4", None));
+        ___qtablewidgetitem4 = self.tv_todo.horizontalHeaderItem(4)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("startfenster", u"spalte 5", None));
     # retranslateUi
 
--- gui.py
+++ gui.py
@@ -3,54 +3,67 @@ from anzeige.frm_main import Ui_startfenster
 from PySide6 import QtSql, QtWidgets
 from PySide6.QtWidgets import QApplication, QMainWindow, QDialog
 from datetime import date
-app = QApplication()
+import sys
+#app = QApplication()
 db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
 db.setDatabaseName("db/database.db")
 class Start_Fenster(QMainWindow, Ui_startfenster):
     def __init__(self):
-        super().__init__()
+        super(Start_Fenster, self).__init__()
         self.setupUi(self)
-        self.mod_todo = QtSql.QSqlRelationalTableModel()
-        self.mod_todo.setTable("todo_liste")
         self.pb_todo_eintragen.clicked.connect(self.todo_eintragen)
         self.todo_aktualisieren()
-        self.tv_todo.setColumnWidth(0, 20)
-        self.tv_todo.setColumnWidth(1, 80)
-        self.tv_todo.setColumnWidth(2, 640)
+        self.tv_todo.setColumnWidth(0, 70)
+        self.tv_todo.setColumnWidth(1, 600)
+        self.tv_todo.setColumnWidth(2, 80)
         self.tv_todo.setColumnWidth(3, 80)
         self.tv_todo.setColumnWidth(4, 80)
-        #self.test_funktionen()("Datum", "Zu erledigen", "Bis wann", "Dringend")
+        self.tv_todo.setColumnWidth(5, 70)
+        self.tv_todo.setHorizontalHeaderLabels(("Datum", "Zu erledigen", "Bis wann", "Dringend", "Erledigt"))
+        self.pb_Projekte.clicked.connect(self.todo_aendern)
 
     def todo_eintragen(self):
         conn = sqlite3.connect("db/database.db")
         cursor = conn.cursor()
-        sql = "INSERT INTO todo_liste (Datum, [zu erledigen], bis_Datum, Dringlichkeit) VALUES (?, ?, ?, ?)"
+        sql = "INSERT INTO todo_liste (Datum, [zu erledigen], bis_Datum, Dringlichkeit, Erledigt) VALUES (?, ?, ?, ?, '0')"
         cursor.execute(sql, (date.today().strftime('%d.%m.%Y'), self.input_text_todo.text(), self.date_end_todo.date().toString(("dd.MM.yyyy")), self.cb_dringlichkeit.currentText(),))
         conn.commit()
         conn.close()
         self.input_text_todo.setText("")
         self.todo_aktualisieren()
     def todo_aktualisieren(self):
-        model = self.mod_todo
-        sqlquery = "SELECT * FROM todo_liste"
-        model.itemData(sqlquery)
-        self.tv_todo.setModel(model)
-        # conn = sqlite3.connect("db/database.db")
-        # cursor = conn.cursor()
-        # sqlquery = "SELECT * FROM todo_liste"
-        # tablerow = 0
-        # for row in cursor.execute(sqlquery):
-        #     self.tv_todo.(tablerow, 0, QtWidgets.QTableWidgetItem(row[0]))
-        #
-        #     tablerow+=1
-        #     print(row)
-        # model = self.mod_todo
-        # model.selectRow()ct()
-        # self.tv_todo.setModel(model)
+        conn = sqlite3.connect("db/database.db")
+        cursor = conn.cursor()
+        sqlquery = "SELECT * FROM todo_liste WHERE Erledigt <> 1"
+        rowcount = 1
+        tablerow = 0
+        for row in cursor.execute(sqlquery):
+            self.tv_todo.setRowCount(rowcount)
+            self.tv_todo.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(row[1]))
+            self.tv_todo.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(row[2]))
+            self.tv_todo.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(row[3]))
+            self.tv_todo.setItem(tablerow, 3, QtWidgets.QTableWidgetItem(row[4]))
+            self.tv_todo.setItem(tablerow, 4, QtWidgets.QTableWidgetItem(row[5]))
+
+            rowcount+=1
+            tablerow+=1
+        conn.close()
+    def todo_aendern(self):
+        for i in self.tv_todo.selectedItems():
+            print(i.text())
+        for i in self.tv_todo.selectedIndexes():
+            print(i.row())
+            print(i.column())
+
     # def test_funktionen(self):
     #     print(self.cb_dringlichkeit.currentText())
 
-
+app = QApplication(sys.argv)
 start_fenster = Start_Fenster()
+
 start_fenster.show()
-app.exec()
+try:
+    sys.exit(app.exec())
+except:
+    print("Exiting")
+
